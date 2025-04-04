76f078dad8c03b4db2d60f1c0bb2d50755f60f24
BrayamChavarro <102553162+BrayamChavarro@users.noreply.github.com>
2025-04-04 09:01:59 -0500

    tu mensaje del commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://brayamchavarro.github.io/', // Tu URL pública está bien
-  base: isProd ? '/brayamchavarro.github.io/' : '/', // Solo usa base en producción
+  site: 'https://brayamchavarro.github.io/',
+  base: '/', // <-- así está bien // Solo usa base en producción
   outDir: 'dist',
 })
 
