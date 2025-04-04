11c6a53d3943171771a0546a2818abab00c4be2f
BrayamChavarro <102553162+BrayamChavarro@users.noreply.github.com>
2025-04-04 07:58:23 -0500

    github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://brayamchavarro.github.io/', // Cambia esto a tu URL de GitHub Pages
+  base: '/brayamchavarro.github.io/', // Asegúrate de usar el nombre del repositorio
+  outDir: 'dist', // Directorio de salida para la construcción
 })
+
