3e9eacec00d2fcea2ccd7ba6592d8ed45c9ca1f2
BrayamChavarro <102553162+BrayamChavarro@users.noreply.github.com>
2025-05-15 09:31:26 -0500

    Ajustar configuración base de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import robotsTxt from "astro-robots-txt";
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://brayamchavarro.github.io',
-  base: '/brayamchavarro.github.io',
+  base: '/',
   output: 'static',
   outDir: 'dist',
 });
